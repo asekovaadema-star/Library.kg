f95e4f278f0c4aa17cd74a248d42dd7053c60fe3
asekovaadema-star <asekovaadema@gmail.com>
2026-08-14 16:10:00 +0600

    hw7

--- myShop/urls.py
+++ myShop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.category_list_view),
-    path('categories/<int:id>/', views.category_detail_view),
+    path('categories/', views.CategoryListView.as_view()),
+    path('categories/<int:id>/', views.CategoryDetailView.as_view()),
 ]
